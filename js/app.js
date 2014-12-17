b41bd9961d856cc9181bce965f090f6777302012
Matt Pedersen <mpedersen2054@gmail.com>
2014-12-16 18:29:03 -0600

    add ajax request method with method to return query string, ajax success calls showplaces method

--- js/app.js
+++ js/app.js
@@ -23,17 +23,44 @@
                 info = [food, city, state]
 
             if ( food && city && state ) {
-                console.log(info)
+                this.getPlaces(info);
             } else {
+                // throw form error, validation
                 alert('not working')
             }
             e.preventDefault();
+        },
+
+        getPlaces: function(locData) {
+            $.ajax({
+                type: 'GET',
+                dataType: 'json',
+                url: getQueryString(),
+                success: $.proxy(this.showPlaces, this),
+                error: function(xhr, status, errorThrown) {
+                    alert('error : ' + errorThrown)
+                    console.log(xhr)
+                }
+            })
+
+            function getQueryString() {
+                var params = locData,
+                    food = params[0],
+                    city = params[1].replace(/ /g,"%20"),
+                    state = params[2],
+                    qString = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20local.search(50)%20where%20query%3D%22'+food+'%22%20and%20location%3D%22'+city+'%2C%20'+state+'%22&format=json&diagnostics=true&callback='
+
+                return qString;
+            }
+        },
+
+        showPlaces: function(data) {
+            console.log('hello')
+            console.log(data)
         }
 
     }
 
     ff.init();
-
-    console.log(ff)
 
 })(jQuery, window);
